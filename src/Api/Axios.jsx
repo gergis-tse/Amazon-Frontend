6fcf8bb54bd3082393397b88aaf8b5b857c135e6
Gergis <gergistse@gmail.com>
2024-12-08 18:58:32 -0700

    amazon frontend deployment

---
+++ src/Api/Axios.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: "https://api-3fh22hukrq-uc.a.run.app",
+  // baseURL:"http://127.0.0.1:5001/clone-24a51/us-central1/api",
+  // baseURL:"https://amazon-api-deploy-37zb.onrender.com",
+  // baseURL: "http://localhost:5000",
+});
+export {axiosInstance};
